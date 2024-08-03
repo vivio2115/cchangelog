225dce4d1e2b486af48b831a100a969c2c3205e4
vi_vio <77623828+vivio2115@users.noreply.github.com>
2024-08-03 21:58:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,20 @@ document.addEventListener("DOMContentLoaded", function() {
                 changelogItem.className = 'changelog-item';
 
                 let icon;
+                let colorClass;
                 if (item.type === 'update') {
                     icon = '<i class="fas fa-star"></i>';
+                    colorClass = 'update';
                 } else if (item.type === 'patch') {
                     icon = '<i class="fas fa-feather-alt"></i>';
+                    colorClass = 'patch';
                 } else if (item.type === 'hotfix') {
                     icon = '<i class="fas fa-tint"></i>';
+                    colorClass = 'hotfix';
                 }
 
                 changelogItem.innerHTML = `
-                    <h2>${icon}${item.version}</h2>
+                    <h2 class="${colorClass}">${icon}${item.version}</h2>
                     <p>${item.description}</p>
                 `;
                 changelogContainer.appendChild(changelogItem);
